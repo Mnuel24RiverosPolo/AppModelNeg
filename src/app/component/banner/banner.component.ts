3fb6010c57e5e3cd61652d9a5b069d1070d48948
SantiagoABM <barbozamujica109@gmail.com>
2024-02-07 15:51:28 -0500

    banner add

---
+++ src/app/component/banner/banner.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent {
+  imgUrls: string[] = [
+    'https://previews.123rf.com/images/sergeypykhonin/sergeypykhonin1705/sergeypykhonin170500095/79248250-compras-banner-de-boutique-concepto-de-tienda-de-moda-ilustraci%C3%B3n-vectorial.jpg',
+    'https://www.shutterstock.com/shutterstock/photos/2210391297/display_1500/stock-vector-website-banner-about-national-wardrobe-day-flat-style-vector-illustration-isolated-on-white-2210391297.jpg',
+
+  ];
+  currentImg: string = '';
+  bannerText: string = "Bienvenidos a Panduro";
+  ngOnInit(): void {
+    this.startImageSlider();
+  }
+
+  startImageSlider() {
+    let index = 0;
+    setInterval(() => {
+      this.bannerText = "Lo último en modas de tu preferencia"
+      this.currentImg = this.imgUrls[index];
+      index = (index + 1) % this.imgUrls.length;
+    }, 4000);
+  }
+}
--- src/app/component/producto/producto.component.ts
+++ src/app/component/producto/producto.component.ts
@@ -8,28 +8,40 @@ import { ProductosService } from 'src/app/services/productos.service';
   styleUrls: ['./producto.component.css']
 })
 export class ProductoComponent {
-  product: any;
-  _id: number = 0
-  constructor(
-    private route: ActivatedRoute,
-    private productService: ProductosService
-  ){}
-  ngOnInit(): void {
-    this.route.params.subscribe(params=>{
-      this._id = params['_id'];
-      console.log(this._id);
-      this.obtenerProductId(this._id)
-    })
+  // product: any;
+  // _id: number = 0
+  // constructor(
+  //   private route: ActivatedRoute,
+  //   private productService: ProductosService
+  // ){}
+  // ngOnInit(): void {
+  //   this.route.params.subscribe(params=>{
+  //     this._id = params['_id'];
+  //     console.log(this._id);
+  //     this.obtenerProductId(this._id)
+  //   })
 
-  }
-  obtenerProductId(id: number){
-    this.product = this.productService.getProductId(id)
-    //.subscribe(
-    //   res => {
-    //     this.product = res;
-    //     console.log(res)
-    //   },
-    //   err => console.log(err)
-    // )
+  // }
+  // obtenerProductId(id: number){
+  //   this.product = this.productService.getProductId(id)
+  //   //.subscribe(
+  //   //   res => {
+  //   //     this.product = res;
+  //   //     console.log(res)
+  //   //   },
+  //   //   err => console.log(err)
+  //   // )
+  // }
+  imgUrls: any[] = [
+    {id: 1, url:'https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_1.jpg'},
+    {id: 2, url:'https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_2.jpg'},
+    {id: 3, url:'https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_3.jpg'},
+    {id: 4, url:'https://fadzrinmadu.github.io/hosted-assets/product-detail-page-design-with-image-slider-html-css-and-javascript/shoe_4.jpg'}
+  ];
+  imgId: number = 1;
+
+  slideImage(id: number) {
+    this.imgId = id-1;
+    console.log(this.imgId);
   }
 }
